62cc1274d9250faa04f3f6ffe7421abfe5d3e157
Juan Camilo Gomez Lopez <31134291+juankg214@users.noreply.github.com>
2019-09-04 10:26:03 -0500

    Bolazani Complete
    Hehe

--- bolzani.m
+++ bolzani.m
@@ -1,29 +1,23 @@
-addpath(pwd);
- function [c] = bolzani(f, a, b, error, itermax)
-  c = (a+b)/2
-  disp("Iteracion    a         b       c     f(a)      f(b)     f(c)")
+function [c] = bolzani(f, a, b, error, itermax)
+  c = (a+b)/2;
+   K = [];
   iter = 0;
   if f(a)*f(b) >= 0
     disp("No hay solucion")
   else
-    while(abs(f(c)) > error)
+    while(abs(f(c)) > error && iter < itermax)
     c = (a+b)/2;
-    K = [iter a b c f(a) f(b) f(c)];
-    disp(k)
+     K = vertcat(K,[iter a b c f(a) f(b) f(c)]);
       if f(a)*f(c) < 0
         b = c;
        elseif f(b)*f(c) < 0
         a = c;
-        end
-      if iter >= itermax
-        disp("failed")
-        break
-        end
+      end
  iter = iter+1;
 endwhile
+disp("    Iteracion      a           b            c         f(a)        f(b)         f(c)")
+disp(K);
 end
 endfunction
-disp("entered")
-f = @(x)2*x^3-x^2+5;
-bolzani(f,-4,-1,0.01)
-
+f = @(x)x*sin(x)-1;
+bolzani(f,0,2,10^-20,100)
