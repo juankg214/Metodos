677b5c8fb21fef52a28cb40f2a51a5c5f1e7c9fe
Juan Camilo Gomez <jcgomezlo@unal.edu.co>
2019-09-06 17:21:32 -0500

    Added Table showing values
    Added Table showing values

--- newton.m
+++ newton.m
@@ -9,10 +9,15 @@ function findZero = newton(funcion,a,itermax)
   epsilon = 10^-10;#Valor de epsilon
   iter = 0;#Contador de iteraciones
   f = inline(funcion, "x"); #Se crea la funcion
+  K = [];
    while(abs(f(findZero)) > epsilon && iter < itermax)# Mientras el valor evualuado en la funcion sea mayor a epsilon, y no haya pasado el limite de iteraciones
-    findZero = findZero - (f(findZero)/fprima(findZero,funcion));#El valor actual menos la division de la funcion evaluada en ese punto sobre la derivada
+    derivada = fprima(findZero,funcion);
+    K = vertcat(K,[iter findZero f(findZero) derivada]); #Añadir nuevos valores al vector
+    findZero = findZero - (f(findZero)/derivada);#El valor actual menos la division de la funcion evaluada en ese punto sobre la derivada
     iter = iter+1;#Se suma la iteracion
   endwhile
+    disp("    Iteracion     C      f(C)       f'(C)")
+    disp(K);
     x = a-16:0.01:a+10;#Se toma el rango de valores de x respecto al punto inciial
     y = arrayfun(f,x); # Se le aplica la funcion f(x) a cada valor del vector
     plot(x,y); #Se grafican los valores
