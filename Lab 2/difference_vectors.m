0fdf0438571f740ed5f708ad8417c9daf47f0104
Juan Camilo Gomez <jcgomezlo@unal.edu.co>
2019-09-18 17:29:03 -0500

    Jacobi
    First Jacobi

---
+++ Lab 2/difference_vectors.m
@@ -0,0 +1,13 @@
+addpath(pwd)
+function c = difference_vectors(x,y)
+  n = size(x)(2);
+  c = true;
+  epsilon = 0.00001;
+  for i = 1:n
+    if abs(x(i)-y(i)) > epsilon
+      c = false;
+      break
+    endif
+  endfor
+endfunction
+
---
+++ Lab 2/jacobi.m
@@ -0,0 +1,34 @@
+function c = jacobi(a,b,x)
+  max_iter = 20;
+  n = size(a)(1);
+  x_aux = zeros(1,n);
+   K = []; #Matriz que imprime.
+   K = vertcat(K, x);
+  for count = 1:max_iter
+	  for i = 1:n
+      aux1 = 0;
+      aux2 = 0;
+      if i != 1
+		    aux1 = dot(a(i,1:i-1),x(1:i-1));
+       endif
+       if i != n
+		    aux2 = dot(a(i,i+1:end),x(i+1:end));
+        endif
+		  x_aux(i) = (b(i) - aux1 - aux2)/a(i,i);
+    endfor
+  if difference(x,x_aux)
+    break
+   endif
+  x = x_aux;
+  K = vertcat(K, x);
+  endfor
+c = x;
+disp(K)
+fprintf("Solucion");
+disp(x)
+endfunction
+
+a = [4,-1,1;4,-8,1;-2,1,5];
+b = [7,-21,15];
+x = [1,2,2];
+jacobi(a,b,x)
