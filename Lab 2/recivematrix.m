1b99a539bfd9347dd33f78dd1ea2804eedb864d1
leotalorac <leotalorac@unal.edu.co>
2019-09-18 15:31:31 -0500

    final details

--- Lab 2/recivematrix.m
+++ Lab 2/recivematrix.m
@@ -1,5 +1,5 @@
 function matrix = recivematrix(n)
-  m =[]
+  m =[];
   for i =1:n
        in=input(cstrcat("element on the row ",mat2str(i)," separated by spaces \n"),"s");
        m = vertcat(m,cellfun("str2num",strsplit(in," ")));
