af665b05f8612215f245ddf6113dcd0ab1734b9e
Juan Camilo Gomez <jcgomezlo@unal.edu.co>
2019-09-18 18:52:12 -0500

    Create eliminacion_gaussiana.m

---
+++ Lab 2/eliminacion_gaussiana.m
@@ -0,0 +1,27 @@
+addpath(pwd)
+function c = eliminacion_gaussiana(a)
+ 
+n = size(a)(1);
+for i = 1:n
+	fila_pivote = zeros(n);
+  counter = i;
+  while fila_pivote(i) == 0
+    if counter <= n
+      fila_pivote = a(counter,1:end); 
+      aux_fila =  a(i,1:end);
+      a(i,1:end) = a(counter,1:end);
+      counter += 1;
+    endif
+  endwhile
+	for j = i+1:n
+		fila_a_transformar = a(j,1:end);
+		fila_pivote_transformada= fila_pivote.*(fila_a_transformar(i)/fila_pivote(i));
+		aux = fila_a_transformar .- fila_pivote_transformada;
+		a(j,1:end) = aux;
+  endfor
+ endfor
+c = a;
+disp(a)
+endfunction
+a = [6,-2,2,4;12,-8,6,10;3,-13,9,3;-6,4,1,-18]
+eliminacion_gaussiana(a);
