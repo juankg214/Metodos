43d5ed524938238aa88c25c6b033c6b049c7710d
leotalorac <leotalorac@unal.edu.co>
2019-09-25 10:15:17 -0500

    finish comment

--- Lab 2/jacobi.m
+++ Lab 2/jacobi.m
@@ -1,29 +1,38 @@
+#funcion para el metodo jacobi
 function c = jacobi(a,b,x)
+  #definimos el numero maximo de iteraciones 
   max_iter = 20;
   n = size(a)(1);
   x_aux = zeros(1,n);
    K = []; #Matriz que imprime.
    printf("Current solution K = 0 : ");
    disp(x);
+  #recorremos hasta el maximo numero de iteraciones
   for count = 1:max_iter
+    #iteramos dentro de la matriz
 	  for i = 1:n
      		aux1 = 0;
       		aux2 = 0;
+          #cuando no es el caso inicial
       		if i != 1
 		    aux1 = dot(a(i,1:i-1),x(1:i-1));
-       		endif
+        endif
+        #cuando no es el cas final 
        		if i != n
 		    aux2 = dot(a(i,i+1:end),x(i+1:end));
         	endif
 		  x_aux(i) = (b(i) - aux1 - aux2)/a(i,i);
     endfor
+  #cuando la diferencia entre los vectores es muy pequeña nos detenemos 
   if difference_vectors(x,x_aux)
     break
    endif
+  #mostrar cada solucion intermedia
   x = x_aux;
   printf("Current solution K = %i", count);
   disp(x);
-  endfor
+endfor
+#imprimir solucion final
 c = x;
 printf("Solucion: \n");
 disp(x);
--- Lab 2/showMatrix.m
+++ Lab 2/showMatrix.m
@@ -1,6 +1,8 @@
+#funcion para imprimir la matriz 
 function m = showMatrix(a,b,n)
   # recibe la martiz a el vector b y la dimension n
   printf("Sistema de Ecuaciones: \n")
+  #recorremos las matrices y las imprimimos como un sistema de ecuaciones
   for i = 1:n
     for j = 1:n
       if j == n
