16ccb4ec60d2eca481e66261a40c5cc4f18971bc
leotalorac <leotalorac@unal.edu.co>
2019-09-25 10:27:04 -0500

    finish comment

--- Lab 2/difference_vectors.m
+++ Lab 2/difference_vectors.m
@@ -5,7 +5,7 @@ function c = difference_vectors(x,y)
   #creamos un vector de tamaño x para recorrer
   c = true;
   #inicializamos la varible boleana en true 
-  epsilon = 0.00001;
+  epsilon = 10^-8;
   #elegimos un epsilon de diferencia optimo
   for i = 1:n
     #recorremos los vectores y comparamos su diferencia con el epsilon
