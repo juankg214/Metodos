7dc2f8d13939d93b3c9aa485fbfb77d3564f8fa7
Juan Camilo Gomez <jcgomezlo@unal.edu.co>
2019-10-07 18:09:43 -0500

    Update dd.m

--- Lab 3/dd.m
+++ Lab 3/dd.m
@@ -8,30 +8,34 @@ function fk = factorial(k)
     fk = factorial(k-1)*k;
   endif
 endfunction
-function cf = createpolinomyal(vector)
+function cf = createpolinomyal(vector,puntosIniciales)
   warning('off', 'all'); # We turn off the warnings, because there are warnings about using double numbers with symbolic
   n = size(vector)(2);
   syms x;
   f =0;
   for i = 1:n
-    printf("iteracion %i \n",i);
-    faux = 1;
-    for j=1:i-1
-      faux *= x-j;  
-      disp(faux);
-    endfor
-    faux = faux/factorial(i-1);
-    if(i!=1)
-      faux = simplify(faux);
-      f += faux*vector(i);
-      f = simplify(f);
+    faux = vector(i);
+    if(i == 1)
+      f += faux;
     else
-      f += faux*vector(i);
+       for j = 1:i-1
+          faux *= (x-puntosIniciales(j));
+       endfor
+       faux = simplify(faux);
+       f += faux;
+       f = simplify(f);
     endif
-    disp(f);
   endfor
-  disp(f);
+  ff = function_handle(f);
   cf = f;
+  xin = puntosIniciales(1)-1:0.001:puntosIniciales(end)+1; # The x-axis points we are going to display
+  y = arrayfun(ff,xin); # we apply the function ff to every value in x, and we get the y-axis 
+  plot(xin,y,"-k");#We plot the x,y
+  hold on;#We are going to add more things to the plot
+  grid on;#We add a grid
+  for i = 1:n#for every value in the vector 
+    plot(puntosIniciales(i),ff(puntosIniciales(i)),"+r");#We plot the point and its image.
+  endfor
 endfunction
 function difft = difftable(x,fun)
   n = size(x)(2);
@@ -42,10 +46,15 @@ function difft = difftable(x,fun)
   endfor
   for j=2:n
     for i=1:n-j+1
-      matrix(i,j) =  matrix(i+1,j-1)-matrix(i,j-1);
+      #rintf("row %i column %i\n",i,j);
+      differenceIndex = x(i+j-1)-x(i);
+      differenceMatrix = matrix(i+1,j-1)-matrix(i,j-1);
+      #sprintf("diff index = %i, diff matr = %i \n",differenceIndex,differenceMatrix)
+      matrix(i,j) =  differenceMatrix/differenceIndex;
     endfor
   endfor 
   disp(matrix);
+  printf("---------------------- \n")
   difft = matrix(1,1:end);
 endfunction
 
@@ -57,10 +66,15 @@ function difftv = difftablevector(x,y)
   endfor
   for j=2:n
     for i=1:n-j+1
-      matrix(i,j) =  matrix(i+1,j-1)-matrix(i,j-1);
+      #rintf("row %i column %i\n",i,j);
+      differenceIndex = x(i+j-1)-x(i);
+      differenceMatrix = matrix(i+1,j-1)-matrix(i,j-1);
+      #sprintf("diff index = %i, diff matr = %i \n",differenceIndex,differenceMatrix)
+      matrix(i,j) =  differenceMatrix/differenceIndex;
     endfor
   endfor 
   disp(matrix);
+  printf("---------------------- \n")
   difftv = matrix(1,1:end);
 endfunction
 
@@ -69,12 +83,12 @@ function dd = DividedDifferences(x,fun)
   difvector = difftable(x,fun);
   disp(difvector);
   printf("---------------------- \n")
-  disp(createpolinomyal(difvector));
+  disp(createpolinomyal(difvector,x));
 endfunction
 
 function dd = DividedDifferencesVector(x,y)
   difvector = difftablevector(x,y);
   disp(difvector);
   printf("---------------------- \n")
-  disp(createpolinomyal(difvector));
+  disp(createpolinomyal(difvector,x));
 endfunction
