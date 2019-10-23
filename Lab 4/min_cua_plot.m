ff1cb6d8e149efc38f40de052f002da5a63a7fd7
SantiMahecha <smahechap@unal.edu.co>
2019-10-23 12:01:58 -0500

    Update min_cua_plot.m

--- Lab 4/min_cua_plot.m
+++ Lab 4/min_cua_plot.m
@@ -1,4 +1,5 @@
 function f=min_cua(t,y)
+  warning('off', 'all'); # We turn off the warnings, because there are warnings about using double numbers with symbolic
   n = 1;#Polynome's grade
   for i = (n+1):-1:1
     A(:,n+2-i)=[t.^(i-1)]';#Each vector is asigned to A columns
