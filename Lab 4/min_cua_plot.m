3e65a542856301e5da2b85937dc25251beb04fe9
SantiMahecha <smahechap@unal.edu.co>
2019-10-23 12:17:43 -0500

    Update min_cua_plot.m

--- Lab 4/min_cua_plot.m
+++ Lab 4/min_cua_plot.m
@@ -6,18 +6,18 @@ function f=min_cua(t,y)
   endfor
   
   M = A' * A;
-  M_i = inv(M);
-  x0 = M_i * A' * y';
+  M_i = inv(M);#inv(M) is the invertible matrix of M
+  x0 = M_i * A' * y';#x0 is the vector with (n+1) polynome coeficients
   
-  EQ = fopen("polynome.m","w");
-  fprintf(EQ,"polynome=");
+  EQ = fopen("polynome.m","w");#We save polynome's expression in polynome.m with octave syntax in order to execute it as a script
+  fprintf(EQ,"polynome=");#With fprintf we print that expression in console
   printf("p(x)=");
 
 	for i = 1:(n+1)
     
-		printf("%+fx^%i",x0(i),n+1-i);
+		printf("%+fx^%i",x0(i),n+1-i); #Define the polynome until grade n
     
-		fprintf(EQ,"%+f.*x.^%i",x0(i),n+1-i);
+		fprintf(EQ,"%+f.*x.^%i",x0(i),n+1-i);#Save polynome un polynome.m
     
 	endfor
 
@@ -26,8 +26,8 @@ function f=min_cua(t,y)
 	fprintf(EQ,";");
 	fclose(EQ);
 
-	x = linspace(min(t)-3,max(t)+3,1000);
-	polynome
+	x = linspace(min(t)-3,max(t)+3,1000);# linspace() returns a row vector with 1000 linearly spaced elements between min(t)-3 and max(t)+3.
+	polynome#We call polynome value from de .m file
 
 	plot(x,polynome,t,y,"*r");
 
