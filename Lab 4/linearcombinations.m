068252017c59414b4da781164a570f61f1d0c5cb
leotalorac <leotalorac@unal.edu.co>
2019-10-30 07:25:12 -0500

    ready funtions and matrix

--- Lab 4/linearcombinations.m
+++ Lab 4/linearcombinations.m
@@ -1,8 +1,27 @@
 function linearcombinations
   points = recivepoints();
   n = size(points)(1);
+  xs=[];
+  ys=[];
+  for i=1:n
+    xs = [xs,points(i,1)];
+    ys = [ys,points(i,2)];
+  endfor
   funs = recivefunctions();
   disp(funs);
   m = size(funs)(2);
   disp(m);
+  f = [];
+  for i=1:n
+    for j=1:m
+      disp(funs{j});
+      fun = inline(funs{j},"x");
+      f(i,j) = fun(xs(j));
+    endfor
+  endfor
+  m = (f')*f;
+  b = (f')/ys;
+  disp(inv(m)*b);
+  #coefficients=Factorizacion_Triangular(m,b);
+ 
 endfunction
